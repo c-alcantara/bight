faeaef8888c4abab2caed0414a513224bbb45115
Christian Alcantara <chrsalctra@icloud.com>
2024-04-14 15:19:37 -0400

    ignore keys

--- src/private/keys.ts
+++
@@ -1,6 +0,0 @@
-export const keys: { [key: string]: string } = {
-    assistantID: 'asst_OmTVVLN4ylLzlqMZrhUCmA2K', // OpenAI Assistant ID
-    openKey: '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', // OpenAI
-    voiceKey: 'acb9bbdfa57637fe3a1c4cfcabfa3c52', //ElevenLabs
-    newsKey: '725fd4f9f8124008b8fc5400631717e0' //newsapi
-};
