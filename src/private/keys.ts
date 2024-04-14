b5439813d5c83f95ab4c349aa035baec98aa57d9
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-04-14 15:07:52 -0400

    Update keys.ts
    Hide

--- src/private/keys.ts
+++ src/private/keys.ts
@@ -1,7 +1,7 @@
 export const keys: { [key: string]: string } = {
-    assistantID: 'asst_OmTVVLN4ylLzlqMZrhUCmA2K', // OpenAI Assistant ID
-    openKey: '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', // OpenAI
-    voiceKey: '0b0bc278d92414622fb60ffa88d42dd0', //ElevenLabs
-    newsKey: '725fd4f9f8124008b8fc5400631717e0' //newsapi
+    assistantID: '', // OpenAI Assistant ID
+    openKey: '', // OpenAI
+    voiceKey: '', //ElevenLabs
+    newsKey: '' //newsapi
 };
   
