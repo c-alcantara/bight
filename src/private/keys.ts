dfbf02302991f2bc676173c4dd4e291ba92709b3
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-04-14 15:21:10 -0400

    Create keys.ts

---
+++ src/private/keys.ts
@@ -0,0 +1,6 @@
+export const keys: { [key: string]: string } = {
+    assistantID: '', // OpenAI Assistant ID
+    openKey: '', // OpenAI
+    voiceKey: '', //ElevenLabs
+    newsKey: '' //newsapi
+};        
