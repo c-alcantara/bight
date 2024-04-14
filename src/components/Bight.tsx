f8459935230841dde3acbebda8477bd49dc55cdc
Christian Alcantara <chrsalctra@icloud.com>
2024-04-14 15:05:57 -0400

    Hide Keys

--- src/components/Bight.tsx
+++ src/components/Bight.tsx
@@ -2,21 +2,14 @@ import Image from 'next/image';
 
 const Bight = () => {
   return (
-
-
-    <div className=" fade-in-main-c transition-all duration-500 mt-20 relative cursor-pointer  transition-all duration-500 '">
-
+    <div className="fade-in-main-c transition-all duration-500 mt-20 relative cursor-pointer">
       <img
         src="/bight.svg"
         alt="Bight.ai"
         width="400px"
-        
       />
-
     </div>
   );
-}  
+}
 
 export default Bight;
-
-
