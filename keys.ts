f5ae8dbd9cb34022618e7d5e83d7a1866496a61e
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-05-04 12:12:28 -0400

    keys

---
+++ keys.ts
@@ -0,0 +1,7 @@
+export const keys: { [key: string]: string } = {
+    assistantID: 'asst_OmTVVLN4ylLzlqMZrhUCmA2K', // OpenAI Assistant ID
+    openKey: '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', // OpenAI
+    voiceKey: 'ac25f62689f3f938937a79a20e1fef19', //ElevenLabs
+    // newsKey: '725fd4f9f8124008b8fc5400631717e0' //newsapi
+};         
+  
