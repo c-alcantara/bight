8e3e36fe139b287b9f145334bcaa91f5dcaf71f3
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-08-14 18:04:32 -0400

    fix silent voice id error

--- src/components/Speak.tsx
+++ src/components/Speak.tsx
@@ -2,17 +2,26 @@ import axios, { AxiosResponse } from 'axios';
 import { keys } from '../../keys';
 import { ElevenLabsClient, ElevenLabs } from "elevenlabs";
 import { voice_ids } from '../private/voice_ids';
-const Speak = async (inputText: string, voiceid: string): Promise<ArrayBuffer> => {
-    
+
+const Speak = async (inputText: string, voiceid: string): Promise<ArrayBuffer | null> => {
     const API_KEY: string = `${keys.voiceKey}`;
+
     try {
-       
         if (voiceid === '') {
-            throw new Error('Voice ID cannot be empty');
+            // Return an empty ArrayBuffer or null if voiceid is empty
+            return null;
         }
+
         const response: AxiosResponse<ArrayBuffer> = await axios.post(
             `https://api.elevenlabs.io/v1/text-to-speech/${voiceid}`,
-            { text: inputText, use_speaker_boost :true, style:30, stability:6, similarity_boost:5, model_id: 'eleven_turbo_v2' },
+            {
+                text: inputText,
+                use_speaker_boost: true,
+                style: 30,
+                stability: 6,
+                similarity_boost: 5,
+                model_id: 'eleven_turbo_v2'
+            },
             {
                 headers: {
                     accept: "audio/mpeg",
@@ -26,7 +35,6 @@ const Speak = async (inputText: string, voiceid: string): Promise<ArrayBuffer> =
         return response.data;
 
     } catch (error) {
-       
         throw error; // Rethrow the error to handle it in the calling code
     }
 };
