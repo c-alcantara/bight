14501717c3eb0bc594fde7a5598527799531637d
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-08-21 00:48:11 -0400

    update logo

--- src/components/Bight.tsx
+++ src/components/Bight.tsx
@@ -4,7 +4,7 @@ const Bight = () => {
   return (
    <div className=" flex justify-center hover:scale-200 transition-transform duration-100  z-55 pb-20 fade-in">
   <img
-    src="/bight.svg"
+    src="/c.svg"
     alt="Bight.ai"
     width="400px"
   />
