f2fb4f67e7ea87bd8df321dfd56b3390a2fee6d0
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-08-31 17:56:21 -0400

    Delete keys.ts

--- keys.ts
+++
@@ -1,7 +0,0 @@
-export const keys: { [key: string]: string } = {
-    assistantID: 'asst_OmTVVLN4ylLzlqMZrhUCmA2K', // OpenAI Assistant ID
-   
-    voiceKey: 'sk_f82513b8faaec76c1d80f1faf6586a077d6a681f1c0d16b0', //ElevenLabs
-    // newsKey: '725fd4f9f8124008b8fc5400631717e0'
-};         
-  
