ba9d361be1f0723636bbded21df8535aba63d274
Christian Elijah Alcantara <chrsalctra@icloud.com>
2024-12-08 23:52:01 -0500

    goo off

--- src/components/Bight.tsx
+++ src/components/Bight.tsx
@@ -1,8 +1,8 @@
 const Bight = () => {
   return (
     <iframe
-      src="https://my.spline.design/interactivesparkletterwithparticleeffect-28e9188418277b13606bb3bc28c4ac6a/"
-      width="940%"
+      src="https://my.spline.design/interactivesparkletterwithparticleeffectcopycopy-bfdb266277c75c20ef9a968772dfa73a/"
+      width="200%"
       height="700%"
     ></iframe>
   ); // Fixed closing of return statement
